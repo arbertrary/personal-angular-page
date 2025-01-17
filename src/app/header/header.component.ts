17d5f84cb8c87ec5aeeff7d5b843faf9284d5f92
Armin Bernstetter <code@arbertrary.dev>
2025-01-17 12:59:57 +0100

    Added initial try at navbar header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 // import { MatToolbarModule } from '@angular/material/toolbar';
+import { trigger, style, query, transition, stagger, animate } from '@angular/animations'
+
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  animations: [
+    trigger("animateMenu", [
+      transition(":enter", [
+        query("*", [
+          style({ opacity: 0, transform: "translateY(-50%)" }),
+          stagger(50, [
+            animate(
+              "250ms cubic-bezier(0.35, 0, 0.25, 1)",
+              style({ opacity: 1, transform: "none" }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
 
