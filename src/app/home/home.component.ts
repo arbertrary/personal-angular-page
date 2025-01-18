e7480a02bbc5868101f42f4f47cb5e93a97316f3
Armin Bernstetter <code@arbertrary.dev>
2025-01-18 18:16:12 +0100

    Added basic portfolio/cv components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { AboutComponent } from "./about/about.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { PublicationsComponent } from "./publications/publications.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent, AboutComponent],
+  imports: [BannerComponent, AboutComponent, ExperienceComponent, ProjectsComponent, PublicationsComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
