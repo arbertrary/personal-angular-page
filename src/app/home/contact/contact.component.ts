186b847d2f389973f60cb79cd29c1c6da2cb30b2
Armin Bernstetter <code@arbertrary.dev>
2025-01-19 21:41:49 +0100

    Add Contact section + fa-regular icons; improve publications section and a few design changes

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  faLinkedin = faLinkedin;
+  faMail = faEnvelopeOpenText;
+
 
 }
--- src/app/home/publications/bibtex-entries.ts
+++ src/app/home/publications/bibtex-entries.ts
@@ -1,70 +1,8 @@
-// export const bibtexEntries = [{
-//     doi: "10.1145/3365523",
-//     title: "a".
-//         authors: "",
-//     venue: "",
-//     year: "",
-//     bibtex: `@article{kobs2020emote,
-//   address = {New York, NY, USA},
-//   articleno = {7},
-//   author = {Kobs, Konstantin and Zehe, Albin and Bernstetter, Armin and Chibane, Julian and Pfister, Jan and Tritscher, Julian and Hotho, Andreas},
-//   doi = {10.1145/3365523},
-//   issue_date = {May 2020},
-//   journal = {Trans. Soc. Comput.},
-//   keywords = {Twitch analysis emotes feedback masterarbeit sentiment},
-//   month = apr,
-//   number = 2,
-//   numpages = {34},
-//   publisher = {Association for Computing Machinery},
-//   title = {Emote-Controlled: Obtaining Implicit Viewer Feedback Through Emote-Based Sentiment Analysis on Comments of Popular Twitch.Tv Channels},
-//   volume = 3,
-//   year = 2020
-// }`}, {
-//     bibtex: `@article{bernstetter2024virtualfieldwork,
-//     title = {Virtual fieldwork in immersive environments using game engines},
-//     journal = {Computers \& Geosciences},
-//     pages = {105855},
-//     year = {2025},
-//     issn = {0098-3004},
-//     doi = {10.1016/j.cageo.2025.105855},
-//     author = {Armin Bernstetter and Tom Kwasnitschka and Jens Karstens and Markus Schlüter and Isabella Peters},
-//     keywords = {Immersive analytics, Virtual fieldwork, Ocean science data, Game engines, Virtual reality, Collaborative},
-// }`}, {
-//     bibtex: `@InProceedings{bernstetter2023practical,
-//   author    = {Bernstetter, Armin and Kwasnitschka, Tom and Peters, Isabella},
-//   booktitle = {EuroVis Workshop on Visual Analytics (EuroVA)},
-//   title     = {{A Practical Approach to Provenance Capturing for Reproducible Visual Analytics at an Ocean Research Institute}},
-//   year      = {2023},
-//   publisher = {The Eurographics Association},
-//   doi       = {10.2312/eurova.20231095},
-// }`}, {
-//     bibtex: `@inproceedings{kwasnitschka2023spatially,
-//   added-at = {2023-07-20T16:27:05.000+0200},
-//   author = {Kwasnitschka, Tom and Schlüter, Markus and Klimmeck, Jens and Bernstetter, Armin and Gross, Felix and Peters, Isabella},
-//   booktitle = {Workshop on Visualisation in Environmental Sciences (EnvirVis)},
-//   doi = {10.2312/envirvis.20231102},
-//   keywords = {facility immersive visualization},
-//   publisher = {The Eurographics Association},
-//   title = {Spatially Immersive Visualization Domes as a Marine Geoscientific Research Tool},
-//   year = 2023
-// }`}, {
-//     bibtex: `@inproceedings{hansen2024softwarevisualizationapproachmultiple,
-//   author={Hansen, Malte and Bielfeldt, Heiko and Bernstetter, Armin and Kwasnitschka, Tom and Hasselbring, Wilhelm},
-//   booktitle={2024 IEEE Working Conference on Software Visualization (VISSOFT)}, 
-//   title={A Software Visualization Approach for Multiple Visual Output Devices}, 
-//   year={2024},
-//   volume={},
-//   number={},
-//   pages={101-105},
-//   keywords={Visualization;Urban areas;Collaboration;Virtual reality;Software systems;Browsers;Synchronization;Monitoring;—software visualization;city metaphor;web;3D;collaborative interaction;program comprehension},
-//   doi={10.1109/VISSOFT64034.2024.00020}}`}
-// ]
-
 export var bibtexEntries = [
   {
     doi: "10.1016/j.cageo.2025.105855",
     title: "Virtual fieldwork in immersive environments using game engines",
-    authors: "Armin Bernstetter; Tom Kwasnitschka; Jens Karstens; Markus Schlüter; Isabella Peters",
+    authors: "Bernstetter, Armin; Kwasnitschka, Tom; Karstens, Jens; Schlüter, Markus; Peters, Isabella",
     venue: "Computers & Geosciences",
     year: 2025,
     bibtex: `@article{bernstetter2024virtualfieldwork,
