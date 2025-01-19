bcaba66088fe73324e585289d71fde4cd71be051
Armin Bernstetter <code@arbertrary.dev>
2025-01-19 23:02:35 +0100

    Add: About Component content

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngular, faJs, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faFlask } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  faAngular = faAngular;
+  faPython = faPython;
+  faFlask = faFlask;
+  faJs = faJs;
+
+
+
 
 }
