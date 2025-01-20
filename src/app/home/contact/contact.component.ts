04a108f51b4b77e5960208a4985e198572f83ea9
Armin Bernstetter <code@arbertrary.dev>
2025-01-20 16:16:49 +0100

    Add 'go up' arrow in contact section

--- src/app/home/contact/contact.component.ts
+++ src/app/home/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faEnvelopeOpenText, faCircleArrowUp } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-contact',
   imports: [FontAwesomeModule],
@@ -12,6 +12,16 @@ export class ContactComponent {
 
   faLinkedin = faLinkedin;
   faMail = faEnvelopeOpenText;
+  faUp = faCircleArrowUp;
 
+  scroll(el: string) {
+    console.log("scroll")
+    if (document.getElementById(el)) {
+      console.log("getElbyID")
+      document.getElementById(el)!.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      // this.router.navigate(['/home']).then(()=> document.getElementById(el).scrollIntoView({behavior: 'smooth'}) );
+    }
 
+  }
 }
