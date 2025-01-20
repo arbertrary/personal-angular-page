460cc06ade4554ded70e03425a0405eb71cdc30d
Armin Bernstetter <code@arbertrary.dev>
2025-01-20 11:33:17 +0100

    Add: Content

--- src/app/home/projects/projects.component.ts
+++ src/app/home/projects/projects.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faGitlab } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+  faGithub = faGithub;
+  faGitlab = faGitlab;
 
 }
