91d6261a8d9075d7d8f41377c73955fceb23778e
Armin Bernstetter <code@arbertrary.dev>
2025-01-21 13:10:03 +0100

    Add: Content to about section

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngular, faJs, faPython } from '@fortawesome/free-brands-svg-icons';
-import { faFlask } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faDragon, faFlask, faGuitar, faMugHot, faScroll, faSeedling } from '@fortawesome/free-solid-svg-icons';
 
 
 @Component({
@@ -15,8 +15,10 @@ export class AboutComponent {
   faPython = faPython;
   faFlask = faFlask;
   faJs = faJs;
-
-
-
-
+  faCoffee = faMugHot;
+  faProgrammer = faCode;
+  faGuitar = faGuitar;
+  faDragon = faDragon;
+  faScroll = faScroll;
+  faFood = faSeedling;
 }
