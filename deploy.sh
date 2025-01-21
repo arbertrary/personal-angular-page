7d4a61e0566c9ca67f1c51a94d1f6d97a85d8e32
Armin Bernstetter <code@arbertrary.dev>
2025-01-21 15:37:23 +0100

    New deploy target

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-ng deploy --base-href=/angular-ghpages-test/ --repo=git@github.com:arbertrary/angular-ghpages-test.git
+# ng deploy --base-href=/angular-ghpages-test/ --repo=git@github.com:arbertrary/angular-ghpages-test.git
+ng deploy --base-href=/ --repo=git@github.com:arbertrary/arbertrary.github.io.git
