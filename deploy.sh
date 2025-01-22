a6eb028bbfcf4c366e8dc579b3053e07e2fed67e
Armin Bernstetter <code@arbertrary.dev>
2025-01-22 10:54:02 +0100

    Fix deploy for custom domain

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # ng deploy --base-href=/angular-ghpages-test/ --repo=git@github.com:arbertrary/angular-ghpages-test.git
-ng deploy --base-href=/ --repo=git@github.com:arbertrary/arbertrary.github.io.git
+ng deploy --cname=www.arbertrary.dev --repo=git@github.com:arbertrary/arbertrary.github.io.git
